4eb901361fa44b23071dffc462014ad886130503
Nadia Di Trapani <nditrapani@gmail.com>
2024-03-27 11:15:18 -0400

    fixed navlink to have the right thing selected when on that page.

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,45 +1,26 @@
 import './header.scss';
 import logo from '../../assets/logo.svg'
 
-import { NavLink , useLocation} from 'react-router-dom';
-import { useState, useEffect } from 'react';
-
+import { NavLink } from 'react-router-dom';
 
 function Header(){
-    const location = useLocation();
-    const [selected, setSelected] = useState(false);
-    
-    useEffect(() => {
-        setSelected(location.pathname === '/gundams' || location.pathname === '/');
-    }, [location]);
-    
-    const handleLogoClick = () => {
-        setSelected(true);
-    };
-
     return(
         <header className='header'>
         <div className='header__logo-container'>
-            <NavLink to={'/'} 
-            onClick={handleLogoClick} 
-            className={`header__logo ${selected ? "active" : ""}`}>
+            <NavLink to={'/'} className='header__logo'>
                 <img src={logo} alt='Gundam Galaxy logo' />
             </NavLink>
         </div>
 
         <ul className='header__container'>
             <li>
-                <NavLink to={'/'} 
-                className={selected ? "active" : ""} 
-                onClick={() => setSelected(true)}>
-                    <p className='header__links'>Catalog</p>
+                <NavLink to={'/'} className='header__links'>
+                    Catalog
                 </NavLink>
             </li>
             <li>
-                <NavLink to={'/user'} 
-                className={!selected ? "active" : ""} 
-                onClick={() => setSelected(false)}>
-                    <p className='header__links'>Profile</p>
+                <NavLink to={'/user'} className='header__links'>
+                    Profile
                 </NavLink>
             </li>
         </ul>
