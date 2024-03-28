937b8c2e0120a26a8adc5ecb4ce004224b7d3a6e
Nadia Di Trapani <nditrapani@gmail.com>
2024-03-28 19:31:04 -0400

    created userOwed section which is now my gundams. displaying all the correct gundams from dummy data

---
+++ src/components/UserOwned/userOwned.js
@@ -0,0 +1,42 @@
+import './userOwned.scss'
+
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+
+function UserOwned(){
+    const { id } = useParams()
+    const [details, setDetails] = useState(null);
+
+    useEffect(() => {
+        const getDetails = async() =>{
+            try {
+                const response = await axios.get(`http://localhost:8080/owned/1`);
+                setDetails(response.data)
+            }catch (err){
+                console.log(err)
+            }
+        }
+        getDetails()
+    }, [id])
+    
+    if (!details){
+        return <div className='retrieving'>Retrieving Gundams...</div>
+    }
+
+    return(
+        <div className='user-list'>
+            <h2 className='user-list__subtitle'>My gundams</h2>
+
+            {details.map(item => (
+                <div key={item.id}>
+                    <img src={item.image} className='user-list__image'/> 
+                    <p>{item.name}</p> 
+                </div>
+            ))}
+        </div>
+    )
+}
+
+
+export default UserOwned;
--- src/components/UserSubNav/userSubNav.js
+++ src/components/UserSubNav/userSubNav.js
@@ -1,20 +1,25 @@
-import UserWishlist from '../UserWishlist/userWishlist';
+import { useState } from 'react';
 import './userSubNav.scss';
+import UserWishlist from '../UserWishlist/userWishlist';
+import UserOwned from '../UserOwned/userOwned';
 
-function UserSubNav(){
-    return(
+function UserSubNav() {
+    const [selectedSection, setSelectedSection] = useState('wishlist'); 
+
+    return (
         <div className='user-sub'>
             <h1 className='user-sub__title'>Welcome Amuro!</h1>
-            
             <div className='user-sub__btn-container'>
-                <button className='user-sub__btn'>Wish List</button>
-                <button className='user-sub__btn'>My Gundams</button>
+                <button className={`user-sub__btn ${selectedSection === 'wishlist' ? 'active' : ''}`} onClick={() => setSelectedSection('wishlist')}>
+                    Wish List
+                </button>
+                <button className={`user-sub__btn ${selectedSection === 'owned' ? 'active' : ''}`} onClick={() => setSelectedSection('owned')}>
+                    My Gundams
+                </button>
             </div>
-
-            <UserWishlist />
+            {selectedSection === 'wishlist' ? <UserWishlist /> : <UserOwned />}
         </div>
-        
-    )
+    );
 }
 
 export default UserSubNav;
