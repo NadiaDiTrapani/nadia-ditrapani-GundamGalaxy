9c8a8cf6b56afc39bed99305df0025e445a9b635
Nadia Di Trapani <nditrapani@gmail.com>
2024-03-29 10:39:30 -0400

    linked gundam in wishlist to details page

--- src/components/UserWishlist/userWishlist.js
+++ src/components/UserWishlist/userWishlist.js
@@ -2,7 +2,7 @@ import './userWishlist.scss';
 
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 function UserWishlist(){
     const { id } = useParams()
@@ -27,13 +27,16 @@ function UserWishlist(){
     return(
         <div className='user-list'>
             <h2 className='user-list__subtitle'>My Wishlist</h2>
-
+            
             {details.map(item => (
+                <Link to={`/gundams/${item.id}`} key ={item.id} className='card-cont__link'>
                 <div key={item.id}>
                     <img src={item.image} className='user-list__image'/> 
                     <p>{item.name}</p> 
                 </div>
+                </Link>
             ))}
+            
         </div>
     )
 }
