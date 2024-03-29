75e7cc68bce7690396cde123c960ddbb302fe7ad
Nadia Di Trapani <nditrapani@gmail.com>
2024-03-29 11:18:56 -0400

    in userowned. seperated gundams into sections

--- src/components/UserOwned/userOwned.js
+++ src/components/UserOwned/userOwned.js
@@ -2,7 +2,7 @@ import './userOwned.scss'
 
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 function UserOwned(){
     const { id } = useParams()
@@ -24,19 +24,50 @@ function UserOwned(){
         return <div className='retrieving'>Retrieving Gundams...</div>
     }
 
-    return(
+    const inProgressGundams = details.filter(item => item.isInProgress);
+    const completedGundams = details.filter(item => item.isCompleted);
+    const otherGundams = details.filter(item => !item.isInProgress && !item.isCompleted);
+
+    return (
         <div className='user-list'>
-            <h2 className='user-list__subtitle'>My gundams</h2>
+            <h2 className='user-list__subtitle'>My Gundams</h2>
 
-            {details.map(item => (
-                <div key={item.id}>
-                    <img src={item.image} className='user-list__image'/> 
-                    <p>{item.name}</p> 
-                </div>
-            ))}
+            <div className='section'>
+                {otherGundams.map(item => (
+                    <Link to={`/gundams/${item.id}`} key={item.id} className='card-cont__link'>
+                        <div>
+                            <img src={item.image} className='user-list__image' alt={item.name} />
+                            <p>{item.name}</p>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+
+            <div className='section'>
+                <h3 className='section__title'>In Progress</h3>
+                {inProgressGundams.map(item => (
+                    <Link to={`/gundams/${item.id}`} key={item.id} className='card-cont__link'>
+                        <div>
+                            <img src={item.image} className='user-list__image' alt={item.name} />
+                            <p>{item.name}</p>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+
+            <div className='section'>
+                <h3 className='section__title'>Completed</h3>
+                {completedGundams.map(item => (
+                    <Link to={`/gundams/${item.id}`} key={item.id} className='card-cont__link'>
+                        <div>
+                            <img src={item.image} className='user-list__image' alt={item.name} />
+                            <p>{item.name}</p>
+                        </div>
+                    </Link>
+                ))}
+            </div>
         </div>
-    )
+    );
 }
-
 
 export default UserOwned;
